5a6be9372446689b571f8dab6c0700f0c777dba6
liminghui <3209498857@qq.com>
2024-03-02 16:23:30 +0800

    完成注册检验

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,22 +5,25 @@
         <h2 class="form-title" v-if="showRegister">注册</h2>
         <h2 class="form-title" v-else>登录</h2>
       </div>
-      <!-- Registration Form -->
+      <!-- 注册-->
       <form v-if="showRegister" @submit.prevent="register" class="form">
         <input
           v-model="registerForm.username"
           class="form-input"
           placeholder="Username"
+          @blur="RegisterUsernameVerification(registerForm)"
         />
         <input
           v-model="registerForm.email"
           class="form-input"
           placeholder="Email"
+          @blur="RegisterEmailVerification(registerForm)"
         />
         <input
           v-model="registerForm.password"
           class="form-input"
           placeholder="Password"
+          @blur="RegisterForPasswordVerification(registerForm)"
           type="password"
         />
         <button type="submit" class="form-button">注册</button>
@@ -29,7 +32,7 @@
           <button @click="toggleForm" class="form-switch-button">登录</button>
         </p>
       </form>
-      <!-- Login Form -->
+      <!-- 登录 -->
       <form v-else @submit.prevent="login" class="form">
         <input
           v-model="loginForm.username"
@@ -95,6 +98,47 @@ function toggleForm() {
 function resetForm() {
   registerForm.value = { username: "", email: "", password: "" }
   loginForm.value = { username: "", password: "" }
+}
+// 注册用户名校验
+const RegisterUsernameVerification = (registerForm) => {
+  if (registerForm.username) {
+    // 校验规则用户名必须以字母开头。用户名可以包含字母、数字、下划线。用户名的长度应该在4到6个字符之间。
+    const usernameRegex = /^[a-zA-Z]\w{3,5}$/
+    if (usernameRegex.test(registerForm.username)) {
+      // 符合规则
+      ElMessage({ message: "用户名正确", type: "success" })
+    } else {
+      ElMessage({
+        message:
+          "用户名无效,用户名必须以字母开头。用户名可以包含字母、数字、下划线。用户名的长度应该在4到6个字符之间",
+        type: "error",
+      })
+    }
+  }
+}
+// 注册邮箱检验
+const RegisterEmailVerification = (registerForm) => {
+  if (registerForm.email) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (emailRegex.test(registerForm.email)) {
+      // 符合规则
+      ElMessage({ message: "邮箱正确", type: "success" })
+    } else {
+      ElMessage({
+        message: "邮箱无效",
+        type: "error",
+      })
+    }
+  }
+}
+// 注册密码校验
+const RegisterForPasswordVerification = (registerForm) => {
+  if (!registerForm.password) {
+    ElMessage({
+      message: "请输入密码",
+      type: "error",
+    })
+  }
 }
 </script>
 <style scoped>
